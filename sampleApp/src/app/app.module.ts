89e7e30fa76c2a758c2901d22433971f3eb8aeb0
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2018-09-10 12:07:10 +0530

    add new component

--- sampleApp/src/app/app.module.ts
+++ sampleApp/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonComponentComponent } from './button-component/button-component.component';
+import { TextInputComponentComponent } from './text-input-component/text-input-component.component';
+import { FormComponentComponent } from './form-component/form-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponentComponent,
+    TextInputComponentComponent,
+    FormComponentComponent
   ],
   imports: [
     BrowserModule
